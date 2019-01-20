01b1a64cc60d012788652dd877a1510aea94551b
brnaldomesi <brnaldomesi@yandex.com>
2019-01-20 08:22:39 +0300

    update schema.sql and record new upgrade

--- storage/database/schema.sql
+++ storage/database/schema.sql
@@ -1541,6 +1541,14 @@ ALTER TABLE `<<prefix>>role_has_permissions`
   ADD CONSTRAINT `<<prefix>>role_has_permissions_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `<<prefix>>roles` (`id`) ON DELETE CASCADE;
 
 
+--verify address
+-- posts
+ALTER TABLE `<<prefix>>posts` ADD `street_name` varchar(255); NULL AFTER `archived_at`;
+ALTER TABLE `<<prefix>>posts` ADD `house_number` varchar(255); NULL AFTER `street_name`;
+ALTER TABLE `<<prefix>>posts` ADD `orientational_number` varchar(255); NULL AFTER `house_number`;
+ALTER TABLE `<<prefix>>posts` ADD `town_district` varchar(255); NULL AFTER `orientational_number`;
+ALTER TABLE `<<prefix>>posts` ADD `town_name` varchar(255); NULL AFTER `town_district`;
+ALTER TABLE `<<prefix>>posts` ADD `zip_code` varchar(255); NULL AFTER `town_name`;
 
 
 SET FOREIGN_KEY_CHECKS=1;
