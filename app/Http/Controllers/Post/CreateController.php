5de2b5419896630bb0fa8e0572cac6423c299147
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-20 13:04:17 +0300

    update address when updating a list

--- app/Http/Controllers/Post/CreateController.php
+++ app/Http/Controllers/Post/CreateController.php
@@ -339,31 +339,4 @@ class CreateController extends FrontController
         return view('post.finish');
     }
 
-    /**
-     * Given address data verify if it is a valid Czech address
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function verifyAddress(Request $request)
-    {
-        $request->validate([
-            'address_street' => 'required'
-        ]);
-        $cr = new CR\CzechRegisters();
-        // get address input
-        $input = array(
-            'street_name' => $request->input('address_street'),
-            'house_number' => $request->input('address_house_no'),
-            'orientational_number' => $request->input('orientational_number'),
-            'town_district' => $request->input('address_town_district'),
-            'town_name' => $request->input('address_town_name'),
-            'zip_code' => $request->input('address_zip_code')
-        );
-        try {
-            $data = $cr->isAddressValid($input);
-            return response()->json(['data' => json_encode($data)]);
-        } catch (Exception $ex) {
-            return response()->json(['data' => json_encode($ex->getMessage())]);
-        }
-    }
 }
--- app/Http/Controllers/Post/Traits/EditTrait.php
+++ app/Http/Controllers/Post/Traits/EditTrait.php
@@ -34,37 +34,37 @@ trait EditTrait
     public function getUpdateForm($postIdOrToken)
     {
         $data = [];
-        
+
         // Get Post
         if (getSegment(2) == 'create') {
             if (!session()->has('tmpPostId')) {
                 return redirect('posts/create');
             }
             $post = Post::with(['city'])
-				->withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
-				->where('id', session('tmpPostId'))
-				->where('tmp_token', $postIdOrToken)
-				->first();
+                ->withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
+                ->where('id', session('tmpPostId'))
+                ->where('tmp_token', $postIdOrToken)
+                ->first();
         } else {
             $post = Post::with(['city'])
-				->withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
-				->where('user_id', auth()->user()->id)
-				->where('id', $postIdOrToken)
-				->first();
+                ->withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
+                ->where('user_id', auth()->user()->id)
+                ->where('id', $postIdOrToken)
+                ->first();
         }
-        
+
         if (empty($post)) {
             abort(404);
         }
-        
+
         view()->share('post', $post);
-        
+
         // Get the Post's Administrative Division
         if (config('country.admin_field_active') == 1 && in_array(config('country.admin_type'), ['1', '2'])) {
             if (!empty($post->city)) {
                 $adminType = config('country.admin_type');
                 $adminModel = '\App\Models\SubAdmin' . $adminType;
-                
+
                 // Get the City's Administrative Division
                 $admin = $adminModel::where('code', $post->city->{'subadmin' . $adminType . '_code'})->first();
                 if (!empty($admin)) {
@@ -72,14 +72,14 @@ trait EditTrait
                 }
             }
         }
-        
+
         // Meta Tags
         MetaTag::set('title', t('Update My Ad'));
         MetaTag::set('description', t('Update My Ad'));
-        
+
         return view('post.edit', $data);
     }
-    
+
     /**
      * Update the Post
      *
@@ -95,98 +95,106 @@ trait EditTrait
                 return redirect('posts/create');
             }
             $post = Post::withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
-				->where('id', session('tmpPostId'))
-				->where('tmp_token', $postIdOrToken)
-				->first();
+                ->where('id', session('tmpPostId'))
+                ->where('tmp_token', $postIdOrToken)
+                ->first();
         } else {
             $post = Post::withoutGlobalScopes([VerifiedScope::class, ReviewedScope::class])
-				->where('user_id', auth()->user()->id)
-				->where('id', $postIdOrToken)
-				->first();
+                ->where('user_id', auth()->user()->id)
+                ->where('id', $postIdOrToken)
+                ->first();
         }
-        
+
         if (empty($post)) {
             abort(404);
         }
-        
+
         // Get the Post's City
         $city = City::find($request->input('city_id', 0));
         if (empty($city)) {
             flash(t("Posting Ads was disabled for this time. Please try later. Thank you."))->error();
-            
+
             return back()->withInput();
         }
-        
+
         // Conditions to Verify User's Email or Phone
         $emailVerificationRequired = config('settings.mail.email_verification') == 1 && $request->filled('email') && $request->input('email') != $post->email;
         $phoneVerificationRequired = config('settings.sms.phone_verification') == 1 && $request->filled('phone') && $request->input('phone') != $post->phone;
-	
-		/*
-		 * Allow admin users to approve the changes,
-		 * If the ads approbation option is enable,
-		 * And if important data have been changed.
-		 */
-		if (config('settings.single.posts_review_activation')) {
-			if (
-				md5($post->title) != md5($request->input('title')) ||
-				md5($post->description) != md5($request->input('description'))
-			) {
-				$post->reviewed = 0;
-			}
-		}
-        
+
+        /*
+         * Allow admin users to approve the changes,
+         * If the ads approbation option is enable,
+         * And if important data have been changed.
+         */
+        if (config('settings.single.posts_review_activation')) {
+            if (
+                md5($post->title) != md5($request->input('title')) ||
+                md5($post->description) != md5($request->input('description'))
+            ) {
+                $post->reviewed = 0;
+            }
+        }
+
         // Update Post
-		$input = $request->only($post->getFillable());
-		foreach ($input as $key => $value) {
-			$post->{$key} = $value;
-		}
+        $input = $request->only($post->getFillable());
+        foreach ($input as $key => $value) {
+            $post->{$key} = $value;
+        }
         $post->negotiable = $request->input('negotiable');
-		$post->phone_hidden = $request->input('phone_hidden');
-		$post->lat = $city->latitude;
+        $post->phone_hidden = $request->input('phone_hidden');
+        $post->lat = $city->latitude;
         $post->lon = $city->longitude;
         $post->ip_addr = Ip::get();
-        
+
         // Email verification key generation
         if ($emailVerificationRequired) {
             $post->email_token = md5(microtime() . mt_rand());
             $post->verified_email = 0;
         }
-        
+
         // Phone verification key generation
         if ($phoneVerificationRequired) {
             $post->phone_token = mt_rand(100000, 999999);
             $post->verified_phone = 0;
         }
-        
+
+        // address
+        $post->street_name = $request->input('address_street');
+        $post->house_number = $request->input('address_house_no');
+        $post->orientational_number = $request->input('orientational_number');
+        $post->town_district = $request->input('address_town_district');
+        $post->town_name = $request->input('address_town_name');
+        $post->zip_code = $request->input('address_zip_code');
+
         // Save
         $post->save();
-    
+
         // Custom Fields
         $this->createPostFieldsValues($post, $request);
-        
+
         // Get Next URL
         $creationPath = (getSegment(2) == 'create') ? 'create/' : '';
-		flash(t("Your ad has been updated."))->success();
-		$nextStepUrl = config('app.locale') . '/posts/' . $creationPath . $postIdOrToken . '/photos';
-        
+        flash(t("Your ad has been updated."))->success();
+        $nextStepUrl = config('app.locale') . '/posts/' . $creationPath . $postIdOrToken . '/photos';
+
         // Send Email Verification message
         if ($emailVerificationRequired) {
             $this->sendVerificationEmail($post);
             $this->showReSendVerificationEmailLink($post, 'post');
         }
-        
+
         // Send Phone Verification message
         if ($phoneVerificationRequired) {
             // Save the Next URL before verification
             session(['itemNextUrl' => $nextStepUrl]);
-            
+
             $this->sendVerificationSms($post);
             $this->showReSendVerificationSmsLink($post, 'post');
-            
+
             // Go to Phone Number verification
             $nextStepUrl = config('app.locale') . '/verify/post/phone/';
         }
-        
+
         // Redirection
         return redirect($nextStepUrl);
     }
