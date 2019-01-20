e540ad3da8fba8a4cef14b78d2e59d75a4fb1a6a
brnaldomesi <brnaldomesi1@gmail.com>
2019-01-20 08:32:31 +0300

    fix typo in schema.sql

--- storage/database/schema.sql
+++ storage/database/schema.sql
@@ -1543,12 +1543,12 @@ ALTER TABLE `<<prefix>>role_has_permissions`
 
 --verify address
 -- posts
-ALTER TABLE `<<prefix>>posts` ADD `street_name` varchar(255); NULL AFTER `archived_at`;
-ALTER TABLE `<<prefix>>posts` ADD `house_number` varchar(255); NULL AFTER `street_name`;
-ALTER TABLE `<<prefix>>posts` ADD `orientational_number` varchar(255); NULL AFTER `house_number`;
-ALTER TABLE `<<prefix>>posts` ADD `town_district` varchar(255); NULL AFTER `orientational_number`;
-ALTER TABLE `<<prefix>>posts` ADD `town_name` varchar(255); NULL AFTER `town_district`;
-ALTER TABLE `<<prefix>>posts` ADD `zip_code` varchar(255); NULL AFTER `town_name`;
+ALTER TABLE `<<prefix>>posts` ADD `street_name` varchar(255) NULL AFTER `archived_at`;
+ALTER TABLE `<<prefix>>posts` ADD `house_number` varchar(255) NULL AFTER `street_name`;
+ALTER TABLE `<<prefix>>posts` ADD `orientational_number` varchar(255) NULL AFTER `house_number`;
+ALTER TABLE `<<prefix>>posts` ADD `town_district` varchar(255) NULL AFTER `orientational_number`;
+ALTER TABLE `<<prefix>>posts` ADD `town_name` varchar(255) NULL AFTER `town_district`;
+ALTER TABLE `<<prefix>>posts` ADD `zip_code` varchar(255) NULL AFTER `town_name`;
 
 
 SET FOREIGN_KEY_CHECKS=1;
